d49ae6c77a9b3eea1a7dbf748146aabd6810d1ac
Yan Oliveira <oliveira.yan02@gmail.com>
2019-02-07 20:04:00 -0200

    alteração validação de senha, exigindo a digitação da nova senha e sua confirmação. Antes estava sendo feita essa verificação no controller, agora foi passado para o validator User.store

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,35 +1,32 @@
-"use strict";
+'use strict'
 
-const User = use("App/Models/User");
-const Hash = use("Hash");
-const Exception = use("App/Exceptions/Exception");
+const User = use('App/Models/User')
+const Hash = use('Hash')
+const Exception = use('App/Exceptions/Exception')
 
 class UserController {
-  async store({ request }) {
-    const data = request.only(["name", "email", "password"]);
-    const user = await User.create(data);
-    return user;
+  async store ({ request }) {
+    const data = request.only(['name', 'email', 'password'])
+    const user = await User.create(data)
+    return user
   }
 
-  async update({ params, request, response, auth, error }) {
-    const user = await User.findByOrFail("id", params.id);
-    const data = request.all();
+  async update ({ params, request, response, auth, error }) {
+    const user = await User.findByOrFail('id', params.id)
+    const data = request.all()
 
-    if (!!data.old_password) {
-      const passIsSame = await Hash.verify(data.old_password, user.password);
+    if (data.old_password) {
+      const passIsSame = await Hash.verify(data.old_password, user.password)
       if (!passIsSame) {
-        throw new Exception("A senha antiga está incorreta!", 400);
+        throw new Exception('A senha antiga está incorreta!', 400)
       }
-      if (!data.new_password) {
-        throw new Exception("Digite a nova senha e a confirmação!", 400);
-      }
-      user.password = data.new_password;
+      user.password = data.new_password
     }
 
-    user.name = data.name;
-    await user.save();
-    return user;
+    user.name = data.name
+    await user.save()
+    return user
   }
 }
 
-module.exports = UserController;
+module.exports = UserController
--- app/Validators/User/Update.js
+++ app/Validators/User/Update.js
@@ -1,24 +1,28 @@
-"use strict";
+'use strict'
 
-const Exception = use("App/Exceptions/Exception");
+const Exception = use('App/Exceptions/Exception')
 
-class UserUpdate extends require("../Validator") {
-  async authorize() {
+class UserUpdate extends require('../Validator') {
+  async authorize () {
     if (this.ctx.auth.user.id != this.ctx.params.id) {
       throw new Exception(
-        "Você não tem permissão para alterar este usuário!",
+        'Você não tem permissão para alterar este usuário!',
         401
-      );
-      return false;
+      )
+      return false
     }
-    return true;
+    return true
   }
 
-  get rules() {
+  get rules () {
     return {
-      new_password: "confirmed"
-    };
+      new_password: `${
+        this.ctx.request.input('old_password')
+          ? 'required|confirmed'
+          : 'confirmed'
+      }`
+    }
   }
 }
 
-module.exports = UserUpdate;
+module.exports = UserUpdate
