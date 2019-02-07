d1b33f1051cccd39d06db232331de52ce12f60e8
Yan Oliveira <oliveira.yan02@gmail.com>
2019-02-07 21:03:40 -0200

    Alteração no UserController.update, para quando for passado o array de tecnologias do usuario. adicionando essa relação na tabela user_technologies

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const User = use('App/Models/User')
+const UserTechnology = use('App/Models/UserTechnology')
 const Hash = use('Hash')
 const Exception = use('App/Exceptions/Exception')
 
@@ -14,6 +15,20 @@ class UserController {
   async update ({ params, request, response, auth, error }) {
     const user = await User.findByOrFail('id', params.id)
     const data = request.all()
+
+    if (data.technologies) {
+      await UserTechnology.query()
+        .where('user_id', auth.user.id)
+        .delete()
+      let technologies = []
+      data.technologies.map(technology => {
+        technologies.push({
+          technology_id: parseInt(technology),
+          user_id: parseInt(auth.user.id)
+        })
+      })
+      await UserTechnology.createMany(technologies)
+    }
 
     if (data.old_password) {
       const passIsSame = await Hash.verify(data.old_password, user.password)
