615fc6f88dc58a18b4c2fda1fe5b06676cce77fb
Yan Oliveira <oliveira.yan02@gmail.com>
2019-02-14 12:50:15 -0200

    Adiçao do transaction na criaçao do meetup.

--- app/Controllers/Http/MeetupController.js
+++ app/Controllers/Http/MeetupController.js
@@ -1,49 +1,52 @@
-'use strict'
+"use strict";
 
-const Meetup = use('App/Models/Meetup')
-const Database = use('Database')
+const Meetup = use("App/Models/Meetup");
+const Database = use("Database");
 
 class MeetupController {
-  async index ({ params, request, response, auth }) {
-    const technologiesUser = Database.select('meetup_id')
-      .from('technology_user')
-      .where('user_id', auth.user.id)
+  async index({ params, request, response, auth }) {
+    const technologiesUser = Database.select("meetup_id")
+      .from("technology_user")
+      .where("user_id", auth.user.id)
       .innerJoin(
-        'meetup_technology',
-        'technology_user.technology_id',
-        'meetup_technology.technology_id'
-      )
+        "meetup_technology",
+        "technology_user.technology_id",
+        "meetup_technology.technology_id"
+      );
 
     const meetups = await Meetup.query()
-      .whereIn('id', technologiesUser)
-      .with('technologies')
-      .fetch()
+      .whereIn("id", technologiesUser)
+      .with("technologies")
+      .fetch();
 
-    return meetups
+    return meetups;
   }
 
-  async store ({ request }) {
+  async store({ request }) {
     const { technologies, ...data } = request.only([
-      'title',
-      'description',
-      'localization',
-      'technologies',
-      'file_id'
-    ])
+      "title",
+      "description",
+      "localization",
+      "technologies",
+      "file_id"
+    ]);
 
-    const meetup = await Meetup.create(data)
-    await meetup.technologies().attach(technologies)
-    await meetup.load('technologies')
+    const trx = await Database.beginTransaction();
 
-    return meetup
+    const meetup = await Meetup.create(data, trx);
+    await meetup.technologies().attach(technologies, null, trx);
+    await trx.commit();
+    await meetup.load("technologies");
+
+    return meetup;
   }
 
-  async show ({ params }) {
-    const meetup = await Meetup.findOrFail(params.id)
-    await meetup.load('technologies')
-    await meetup.load('users')
-    return meetup
+  async show({ params }) {
+    const meetup = await Meetup.findOrFail(params.id);
+    await meetup.load("technologies");
+    await meetup.load("users");
+    return meetup;
   }
 }
 
-module.exports = MeetupController
+module.exports = MeetupController;
